0c3722a4f13faaeb637bc6a0bd92514f44056c03
DZuko <dcouser.dev@gmail.com>
2022-01-22 20:03:09 -0500

    started to add tests

--- src/test/ArbL2Messenger.t.sol
+++ src/test/ArbL2Messenger.t.sol
@@ -2,11 +2,32 @@
 pragma solidity >=0.8.0;
 
 import "../../lib/ds-test/src/test.sol";
+import "../ArbL2Messenger.sol";
 
 contract ArbL2MessengerTest is DSTest {
-    function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    arbL2Messenger ArbL2Messenger;
+
+
+    function setUp() public {
+        //create a new ArbL2Messenger
+        arbL2Messenger = new ArbL2Messenger();
+    }
+
+
+    function testSendMessageToL2() public {
+        
+
+        //define a target L2 address
+        address targetL2Address=0xE592427A0AEce92De3Edee1F18E0157C05861564;
+        //create data and encode into bytes
+        //set max submission cost
+
+        //set max gas
+
+        //set gas price bid
+
+        retryableTicket =arbL2Messenger.sendMessageToL2(targetL2Address, data, l2CallValue, maxSubmissionCost, maxGas, gasPriceBid);
+
     }
 }
